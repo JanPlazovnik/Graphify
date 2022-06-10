688582a8c835aacac806a47c4c818a217952d1b6
Jan Plazovnik <jan.plazovnik@gmail.com>
2022-06-11 01:34:50 +0200

    Added Spotify api calls

---
+++ src/hooks/useApi.ts
@@ -0,0 +1,31 @@
+import axios, { AxiosResponse } from 'axios';
+import { useRecoilState } from 'recoil';
+import { tokenState } from '../state/recoil';
+import { ArtistsResponse, TimeRange, TracksResponse } from '../types/spotify';
+
+export default function useApi() {
+    const [token,] = useRecoilState(tokenState);
+
+    class SpotifyService {
+        public getAuthLink(): string {
+            return `https://accounts.spotify.com/authorize?` + [
+                `client_id=${import.meta.env.VITE_APP_SPOTIFY_CLIENT_ID}`,
+                `redirect_uri=${import.meta.env.VITE_APP_SPOTIFY_REDIRECT_URI}`,
+                `scope=${import.meta.env.VITE_APP_SPOTIFY_SCOPE}`,
+                `response_type=token`
+            ].join("&");
+        }
+
+        public artists(limit = 10, time_range: TimeRange = "medium_term"): Promise<AxiosResponse<ArtistsResponse>> {
+            return axios.get(`https://api.spotify.com/v1/me/top/artists?limit=${limit}&time_range=${time_range}`, { headers: { 'Authorization': `Bearer ${token}` } });
+        }
+
+        public tracks(limit = 10, time_range: TimeRange = "medium_term"): Promise<AxiosResponse<TracksResponse>> {
+            return axios.get(`https://api.spotify.com/v1/me/top/tracks?limit=${limit}&time_range=${time_range}`, { headers: { 'Authorization': `Bearer ${token}` } });
+        }        
+    }
+
+    return {
+        spotify: new SpotifyService()
+    }
+}
