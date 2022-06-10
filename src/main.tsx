ae548c0357efc56106e42754418215116fbe62d9
Jan Plazovnik <jan.plazovnik@gmail.com>
2022-06-11 01:34:13 +0200

    Added content wrappers and dayjs

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
 import App from './App';
 import './assets/styles/global.css' ;
+import dayjs from "dayjs";
+import duration from 'dayjs/plugin/duration'
+import relativeTime from 'dayjs/plugin/relativeTime'
+
+dayjs.extend(duration);
+dayjs.extend(relativeTime);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <App />
+        <RecoilRoot>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </RecoilRoot>
     </React.StrictMode>
 );
