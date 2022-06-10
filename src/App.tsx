438c630442ad9a83be40a0f8ed608a21a3c9aaf5
Jan Plazovnik <jan.plazovnik@gmail.com>
2022-06-11 01:36:18 +0200

    Added routes and session validation

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,56 @@
+import { useMemo, useEffect } from 'react';
+import { Navigate, Route, Routes } from 'react-router';
+import { useRecoilState } from 'recoil';
+import Login from './pages/auth/Login';
+import Profile from './pages/Profile';
+import { expirationState, tokenState } from './state/recoil';
+import Callback from './pages/auth/Callback';
+import dayjs from 'dayjs';
+import Logout from './pages/auth/Logout';
+
 function App() {
+    const [token, setToken] = useRecoilState(tokenState);
+    const [expiration, setExpiration] = useRecoilState(expirationState);
+
+    useEffect(() => {
+        if (!token || !expiration) return;
+
+        if (dayjs(expiration).diff(dayjs(), 's') <= 0) {
+            console.log("Session expired.");
+
+            setToken(null);
+            setExpiration(null);
+            localStorage.removeItem("token");
+            localStorage.removeItem("expiration");
+        }
+    }, []);
+
+    const routes = useMemo(() => {
+        // If not authorized, redirect to login
+        if (!token) return (
+            <>
+                <Route path="/login" element={<Login/>}/>
+                <Route path="/callback" element={<Callback/>}/>
+                <Route path="/*" element={<Navigate replace to="/login"/>}/>
+            </>
+        )
+
+        return (
+            <>
+                <Route path="/" element={<Profile/>}/>
+                <Route path="/logout" element={<Logout/>}/>
+                <Route path="/*" element={<Navigate replace to="/"/>}/>
+            </>
+        )
+    }, [token]);
+
     return (
-        <h1 className='text-3xl font-semibold text-indigo-500'>Hello, world!</h1>
-    );
+        <div className='bg-dark-200 flex flex-col p-3 h-screen overflow-y-auto'>
+            <Routes>
+                {routes}
+            </Routes>
+        </div>
+    )
 }
 
 export default App;
