a6810fd18401f78cffc0ce1e21176d0eada6f5c9
Jan Plazovnik <jan.plazovnik@gmail.com>
2022-06-11 20:28:08 +0200

    Updated top 3 colors

--- src/components/ArtistPanel.tsx
+++ src/components/ArtistPanel.tsx
@@ -1,10 +1,16 @@
 import { Artist } from '../types/spotify';
-import { FiExternalLink } from 'react-icons/fi';
 
 export default function ArtistPanel({artist, order}: {artist: Artist, order: number}) {
+    function getBadgeColor(): string {
+        if (order == 1) return 'bg-yellow-500'
+        if (order == 2) return 'bg-stone-400'
+        if (order == 3) return 'bg-amber-700'
+        return 'bg-accent-100'
+    }
+
     return (
         <div className='flex items-center gap-5 bg-dark-100 w-full p-5 my-2 rounded-lg shadow-md'>
-            <div className='bg-accent-100 min-w-[50px] min-h-[50px] flex items-center justify-center font-bold text-2xl rounded-full'>{order}</div>
+            <div className={`${getBadgeColor()} min-w-[50px] min-h-[50px] flex items-center justify-center font-bold text-2xl rounded-full`}>{order}</div>
             <div className='inline-flex flex-col gap-1 items-start'>
                 <p className='text-2xl font-semibold'>{artist.name}</p>
                 {/* <a 
@@ -16,6 +22,7 @@ export default function ArtistPanel({artist, order}: {artist: Artist, order: num
                     <FiExternalLink className='text-[0.9rem]'/>Spotify
                 </a> */}
             </div>
+            <img src={artist.images[1].url} className='ml-auto max-w-[50px] rounded-full' />
         </div>
     )
 }
--- src/components/TrackPanel.tsx
+++ src/components/TrackPanel.tsx
@@ -1,14 +1,21 @@
-import { Artist, Track, TrackArtist } from '../types/spotify';
-import { FiExternalLink } from 'react-icons/fi';
+import { Track, TrackArtist } from '../types/spotify';
 
 export default function TrackPanel({ track, order }: { track: Track, order: number }) {
+    function getBadgeColor(): string {
+        if (order == 1) return 'bg-yellow-500'
+        if (order == 2) return 'bg-stone-400'
+        if (order == 3) return 'bg-amber-700'
+        return 'bg-accent-100'
+    }
+
     return (
         <div className='flex items-center gap-5 bg-dark-100 w-full p-5 my-2 rounded-lg shadow-md'>
-            <div className='bg-accent-100 min-w-[50px] min-h-[50px] flex items-center justify-center font-bold text-2xl rounded-full'>{order}</div>
+            <div className={`${getBadgeColor()} min-w-[50px] min-h-[50px] flex items-center justify-center font-bold text-2xl rounded-full`}>{order}</div>
             <div className='inline-flex flex-col gap-1 items-start'>
                 <p className='text-2xl font-semibold'>{track.name}</p>
                 <p className='text-1xl'>{track.artists.map((artist: TrackArtist) => artist.name).join(", ")}</p>
             </div>
+            <img src={track.album.images[1].url} className='ml-auto max-w-[50px] rounded-full' />
         </div>
     )
 }
