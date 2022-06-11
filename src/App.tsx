49b13966140e890398fca9a12da592fcc58ffdfc
Jan Plazovnik <jan.plazovnik@gmail.com>
2022-06-11 13:34:47 +0200

    Updated app routes and added header on all pages

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,9 @@ import { expirationState, tokenState } from './state/recoil';
 import Callback from './pages/auth/Callback';
 import dayjs from 'dayjs';
 import Logout from './pages/auth/Logout';
+import Header from './components/navigation/Header';
+import TopArtists from './pages/private/TopArtists';
+import TopTracks from './pages/private/TopTracks';
 
 function App() {
     const [token, setToken] = useRecoilState(tokenState);
@@ -38,6 +41,8 @@ function App() {
         return (
             <>
                 <Route path="/" element={<Profile/>}/>
+                <Route path="/artists" element={<TopArtists/>}/>
+                <Route path="/tracks" element={<TopTracks/>}/>
                 <Route path="/logout" element={<Logout/>}/>
                 <Route path="/*" element={<Navigate replace to="/"/>}/>
             </>
@@ -45,11 +50,14 @@ function App() {
     }, [token]);
 
     return (
-        <div className='bg-dark-200 flex flex-col p-3 h-screen overflow-y-auto'>
-            <Routes>
-                {routes}
-            </Routes>
-        </div>
+        <>
+            <Header />
+            <div className='bg-dark-200 flex flex-col p-3 h-screen overflow-y-auto'>
+                <Routes>
+                    {routes}
+                </Routes>
+            </div>
+        </>
     )
 }
 
