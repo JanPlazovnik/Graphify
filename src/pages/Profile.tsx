7be48b61ee9c39c22522a552004f6256d58dcd44
Jan Plazovnik <jan.plazovnik@gmail.com>
2022-06-11 20:28:01 +0200

    Added icon and formatted code

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,14 +1,15 @@
 import { useState, useEffect } from 'react';
 import { SpotifyProfile } from '../types/spotify';
 import useApi from '../hooks/useApi';
+import { FiExternalLink } from 'react-icons/fi';
 
 export default function Profile() {
     const api = useApi();
-    const [profile, setProfile] = useState<SpotifyProfile|null>(null);
+    const [profile, setProfile] = useState<SpotifyProfile | null>(null);
 
     async function fetchProfileData() {
         try {
-            const {data} = await api.spotify.me();
+            const { data } = await api.spotify.me();
             setProfile(data);
         }
         catch (e: any) {
@@ -26,7 +27,7 @@ export default function Profile() {
             <h1 className='text-3xl text-white font-bold'>{profile?.display_name}</h1>
             {/* Followers Badge */}
             <span className='px-2 py-1 font-semibold bg-dark-100 rounded-md'>{profile?.followers.total} followers</span>
-            <a href={profile?.external_urls.spotify} className='px-2 py-1 font-semibold bg-dark-100 transition hover:bg-accent-200 hover:text-dark-100 rounded-md'>Follow on Spotify</a>
+            <a href={profile?.external_urls.spotify} target="_blank" rel='noreferrer' className='flex flex-row items-center justify-center gap-2 px-2 py-1 font-semibold bg-dark-100 transition hover:opacity-[0.9] rounded-md'><FiExternalLink />Follow on Spotify</a>
         </div>
     )
 }
