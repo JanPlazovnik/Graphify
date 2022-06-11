b5f3c1ddf714ac679b9d981af1537ed2135ed00c
Jan Plazovnik <jan.plazovnik@gmail.com>
2022-06-11 13:34:19 +0200

    Added extra information on login screen

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -5,14 +5,18 @@ export default function Login() {
     const api = useApi();
 
     return (
-        <div className='h-full flex items-center justify-center py-12 px-4'>
-            <a 
-                href={api.spotify.getAuthLink()} 
+        <div className='h-full flex flex-col items-start justify-center py-12 gap-3 container max-w-4xl mx-auto p-3'>
+            {/* Message */}
+            <h1 className='text-4xl font-bold text-white'>Your Spotify stats &ndash; anywhere, anytime.</h1>
+            <p className='text-lg text-gray-400'>View details about your favourite tracks and artists in the past <span className='utext'>month</span>, <span className='utext'>6 months</span> or <span className='utext'>all time</span>.</p>
+            {/* Login Button */}
+            <a
+                href={api.spotify.getAuthLink()}
                 className='flex items-center justify-center gap-[10px] bg-accent-200 text-dark-100 text-1xl px-5 py-3 rounded-lg shadow font-semibold transition hover:scale-105'
             >
-                <BsSpotify className='text-3xl text-white'/>
-                Login to Spotify
-            </a>
+                <BsSpotify className='text-3xl' />
+                Login with Spotify
+            </a>   
         </div>
     )
 }
